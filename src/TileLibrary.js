c36dd4ef00cecc0d82659597db5aec93f3503eb6
Jay Williams <jay@freebuddhistaudio.com>
2020-05-01 00:01:04 +0100

    Add more tiles. Fix syntax of TileLibrary

--- src/TileLibrary.js
+++ src/TileLibrary.js
@@ -4,55 +4,77 @@ export default {
     return this.packed.map((x) => x.tile)
   },
   packed: [
-    { 'label': 'Cloister',
-      'count': 4,
-      'tile': {
-        'sides': [ 'g', 'g', 'g', 'g' ],
-        'cloister': 1
+    { label: 'Cloister',
+      count: 4,
+      tile: {
+        sides: [ 'g', 'g', 'g', 'g' ],
+        cloister: 1
       }
     },
 
-    { 'label': 'Cloister with road',
-      'count': 2,
-      'tile': {
-        'sides': [ 'g', 'g', 'r', 'g' ],
-        'cloister': 1
+    { label: 'Single city',
+      count: 5,
+      tile: {
+        sides: [ 'c', 'g', 'g', 'g' ]
       }
     },
 
-    { 'label': 'Corner city with corner road',
-      'count': 3,
-      'tile': {
-        'sides': [ 'c', 'c', 'r', 'r' ]
+    { label: 'Bridge city',
+      count: 1,
+      tile: {
+        sides: [ 'g', 'c', 'g', 'c' ]
       }
     },
 
-    { 'label': 'Bonus corner city with corner road',
-      'count': 2,
-      'tile': {
-        'sides': [ 'c', 'c', 'r', 'r' ],
-        'bonus': 1
+    { label: 'Bonus bridge city',
+      count: 2,
+      tile: {
+        sides: [ 'g', 'c', 'g', 'c' ]
+      },
+      bonus: 1
+    },
+
+    { label: 'Cloister with road',
+      count: 2,
+      tile: {
+        sides: [ 'g', 'g', 'r', 'g' ],
+        cloister: 1
       }
     },
 
-    { 'label': 'Three-way cross-road',
-      'count': 4,
-      'tile': {
-        'sides': [ 'g', 'r', 'r', 'r' ]
+    { label: 'Corner city with corner road',
+      count: 3,
+      tile: {
+        sides: [ 'c', 'c', 'r', 'r' ]
       }
     },
 
-    { 'label': 'Three-way cross-road with city',
-      'count': 3,
-      'tile': {
-        'sides': [ 'c', 'r', 'r', 'r' ]
+    { label: 'Bonus corner city with corner road',
+      count: 2,
+      tile: {
+        sides: [ 'c', 'c', 'r', 'r' ],
+        bonus: 1
       }
     },
 
-    { 'label': 'Four-way cross-road',
-      'count': 2,
-      'tile': {
-        'sides': [ 'r', 'r', 'r', 'r' ]
+    { label: 'Three-way cross-road',
+      count: 4,
+      tile: {
+        sides: [ 'g', 'r', 'r', 'r' ]
+      }
+    },
+
+    { label: 'Three-way cross-road with city',
+      count: 3,
+      tile: {
+        sides: [ 'c', 'r', 'r', 'r' ]
+      }
+    },
+
+    { label: 'Four-way cross-road',
+      count: 2,
+      tile: {
+        sides: [ 'r', 'r', 'r', 'r' ]
       }
     }
   ]
