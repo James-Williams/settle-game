0529f444f9b9847e03a52b5ac4b8108b2d39de53
Jay Williams <jay@freebuddhistaudio.com>
2020-05-01 18:54:48 +0100

    Random playing working

---
+++ src/components/RandomTileDemo.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="page">
+    <Tile @clicked="rotate(pickedTile)" :type="pickedTile" />
+    <div>
+      <Grid @clicked="place" :tiles="grid" />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Tile from './Tile'
+import TileLibrary from '@/TileLibrary'
+import TilePicker from './TilePicker'
+import Grid from './Grid'
+
+export default {
+  data () {
+    return {
+      pickedTile: {
+        sides: [ 'g', 'g', 'g', 'g' ]
+      },
+      tiles: TileLibrary.uniqueTiles().map(function (x) { return {...x, selectable: 1} }),
+      grid: { [String([0, 0])]: { sides: ['c', 'r', 'g', 'r' ], selectable: 1 } }
+    }
+  },
+  methods: {
+    randomizePick () {
+      const idx = Math.floor(Math.random() * this.tiles.length)
+      console.log(idx)
+      this.pickedTile = this.tiles[idx]
+    },
+    rotate (tile) {
+      const ss = []
+      for (var i = 0; i < tile.sides.length; i++) {
+        ss.push(tile.sides[(i + tile.sides.length - 1) % tile.sides.length])
+      }
+      tile.sides = ss
+    },
+    place (pos) {
+      if (this.pickedTile) {
+        const newTile = JSON.parse(JSON.stringify(this.pickedTile))
+        var grid = {...this.grid, [String(pos)]: newTile}
+        this.grid = grid
+      }
+      this.randomizePick()
+    }
+  },
+  created () {
+    this.randomizePick()
+  },
+  components: {
+    Tile,
+    TilePicker,
+    Grid
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.page {
+  div {
+    margin-bottom: 15px;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import DemoPage from '@/components/DemoPage'
+import RandomTileDemo from '@/components/RandomTileDemo'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       component: DemoPage
+    },
+    {
+      path: '/random',
+      component: RandomTileDemo
     }
   ]
 })
