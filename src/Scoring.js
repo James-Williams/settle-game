756c91d392633e3933bfbbb4549588e2e39d3b75
Jay Williams <jay@freebuddhistaudio.com>
2020-05-09 18:10:53 +0100

    Graph functions added for meeple slots - freeSlots tests FAILING

--- src/Scoring.js
+++ src/Scoring.js
@@ -2,6 +2,114 @@
 import Moves from '@/Moves'
 
 export default class {
+
+  static gridGraph(grid) {
+    const adj = {}
+    const nodes = {}
+
+    grid.keys().forEach((pos) => {
+      const tile = grid.get(pos)
+      const g = this.globalTileGraph(tile, pos)
+      Object.keys(g.nodes).forEach((key) => {
+        nodes[key] = g.nodes[key]
+        adj[key] = g.adj[key]
+      })
+
+      for (let i = 0; i < 4; i++) {
+        const dir = Moves.DIRECTIONS[i]
+        const eastKey = [pos[0] + dir[0], pos[1] + dir[1]]
+        const eastTile = grid.get(eastKey)
+        if (eastTile) {
+          // Todo - Fix for farm connections
+          const adjDir = [-1 * dir[0], -1 * dir[1]]
+          const nodeKey = String(pos) + ',' + String(dir)
+          const adjKey = String(eastKey) + ',' + String(adjDir)
+          adj[nodeKey].add(adjKey)
+        }
+      }
+    })
+
+    return { nodes: nodes, adj: adj }
+  }
+
+  static partitionGraph(graph) {
+    const partitions = {}
+    const membership = {}
+    let nextPartition = 0
+
+    Object.keys(graph.nodes).forEach((nodeKey) => {
+      let p = null
+      graph.adj[nodeKey].forEach((adjKey) => {
+        if (adjKey in membership) {
+          if (p !== null && p !== membership[adjKey]) {
+            throw new Error('Clashing partitions')
+          }
+          p = membership[adjKey]
+        }
+      })
+      if (p === null) {
+        p = nextPartition++
+        partitions[p] = new Set()
+      }
+      partitions[p].add(nodeKey)
+      membership[nodeKey] = String(p)
+    })
+
+    return { partitions: partitions, membership: membership }
+  }
+
+  static globalTileGraph(tile, position) {
+    const tg = this.tileGraph(tile)
+    const nodes = {}
+    const adj = {}
+
+    Object.keys(tg.nodes).forEach((key) => {
+      const node = tg.nodes[key]
+      const newKey = String(position) + ',' + key
+      nodes[newKey] = { ...node, pos: position }
+
+      adj[newKey] = new Set()
+      tg.adj[key].forEach((target) => {
+        adj[newKey].add(String(position) + ',' + target)
+      })
+    })
+
+    return {adj: adj, nodes: nodes}
+  }
+
+  static tileGraph (tile) {
+    const adj = {}
+    const nodes = {}
+
+    for (let i = 0; i < 4; i++) {
+      const prevIdx = (i === 0) ? 3 : i - 1
+      const nextIdx = (i === 3) ? 0 : i + 1
+      const side = tile.sides[i]
+      const vec = Moves.DIRECTIONS[i]
+      const set = new Set()
+      if (tile.sides[prevIdx] === side) {
+        set.add(String(Moves.DIRECTIONS[prevIdx]))
+      }
+      if (tile.sides[nextIdx] === side) {
+        set.add(String(Moves.DIRECTIONS[nextIdx]))
+      }
+      adj[String(vec)] = set
+
+      nodes[String(vec)] = { type: side, ofst: vec }
+      if (tile.meeple && String(vec) === String(tile.meeple.position)) {
+      nodes[String(vec)].meeple = {...tile.meeple}
+      }
+    }
+
+    return { adj: adj, nodes: nodes }
+  }
+
+  static freeSlots (grid, pos) {
+    const tile = grid.get(pos)
+    return this.meepleSlots(tile)
+  }
+
+  // TODO - Replace this with a call to tileGraph with Object.keys
   static meepleSlots (tile) {
     const res = []
 
--- test/unit/specs/Scoring.spec.js
+++ test/unit/specs/Scoring.spec.js
@@ -1,4 +1,167 @@
 import Scoring from '@/Scoring'
+import Grid from '@/Grid'
+
+describe('gridGraph', () => {
+
+  it('two cites are connected', () => {
+    const fullCity = { sides: ['c', 'c', 'c', 'c'] }
+
+    const grid = new Grid({
+      [String([0,0])]: fullCity,
+      [String([1,0])]: fullCity
+    })
+
+    const graph = Scoring.gridGraph(grid)
+
+    expect(graph.adj[String([0,0,1,0])])
+      .toContain(String([1,0,-1,0]))
+    expect(graph.adj[String([0,0,1,0])].size)
+      .toEqual(3)
+    expect(graph.adj[String([1,0,-1,0])])
+      .toContain(String([0,0,1,0]))
+    expect(graph.adj[String([1,0,-1,0])].size)
+      .toEqual(3)
+  })
+})
+
+describe('partitionGraph', () => {
+
+  it('should find partitions on generic graph', () => {
+    const graph = {
+      nodes: {
+        0: {a:'b'},
+        1: {c:'d'},
+        A: {1:2}
+      },
+      adj: {
+        0: new Set(['1']),
+        1: new Set(['0']),
+        A: new Set()
+      }
+    }
+
+    expect(Scoring.partitionGraph(graph))
+      .toEqual({
+        partitions: {
+          0: new Set(['0', '1']),
+          1: new Set(['A'])
+        },
+        membership: {
+          0: '0',
+          1: '0',
+          A: '1'
+        }
+      })
+  })
+})
+
+describe('globalTileGraph', () => {
+
+  it('Graph for full city with meeple', () => {
+    const fullCity = {
+      sides: ['c', 'c', 'c', 'c'],
+      meeple: { position: [0, 1], color: 'orange' }
+    }
+
+    expect(Scoring.globalTileGraph(fullCity, [1,1]))
+      .toEqual({
+        adj: {
+          [String([1,1, 1,0])]: new Set([ String([1,1,0,-1]), String([1,1,0,1]) ]),
+          [String([1,1,-1,0])]: new Set([ String([1,1,0,-1]), String([1,1,0,1]) ]),
+          [String([1,1,0, 1])]: new Set([ String([1,1,-1,0]), String([1,1,1,0]) ]),
+          [String([1,1,0,-1])]: new Set([ String([1,1,-1,0]), String([1,1,1,0]) ])
+        },
+        nodes: {
+          [String([1,1, 1,0])]: { type: 'c', pos: [1,1], ofst: [1,0] },
+          [String([1,1,-1,0])]: { type: 'c', pos: [1,1], ofst: [-1,0] },
+          [String([1,1,0, 1])]: { type: 'c', pos: [1,1], ofst: [0,1], meeple: {...fullCity.meeple} },
+          [String([1,1,0,-1])]: { type: 'c', pos: [1,1], ofst: [0,-1] }
+        }
+      })
+  })
+})
+
+describe('tileGraph', () => {
+
+  it('Graph for full city with meeple', () => {
+    const fullCity = {
+      sides: ['c', 'c', 'c', 'c'],
+      meeple: { position: [0, 1], color: 'orange' }
+    }
+
+    expect(Scoring.tileGraph(fullCity))
+      .toEqual({
+        adj: {
+          [String([ 1,0])]: new Set([ String([0,-1]), String([0,1]) ]),
+          [String([-1,0])]: new Set([ String([0,-1]), String([0,1]) ]),
+          [String([0, 1])]: new Set([ String([-1,0]), String([1,0]) ]),
+          [String([0,-1])]: new Set([ String([-1,0]), String([1,0]) ])
+        },
+        nodes: {
+          [String([ 1,0])]: { type: 'c', ofst: [1,0] },
+          [String([-1,0])]: { type: 'c', ofst: [-1,0] },
+          [String([0, 1])]: { type: 'c', ofst: [0,1], meeple: {...fullCity.meeple} },
+          [String([0,-1])]: { type: 'c', ofst: [0,-1] }
+        }
+      })
+  })
+})
+
+describe('freeSlots', () => {
+
+  it('all slots free on single tile', () => {
+    const fullCity = { sides: ['c', 'c', 'c', 'c'] }
+
+    const grid = new Grid({
+      [String([0,0])]: fullCity
+    })
+
+    expect(Scoring.freeSlots(grid, [0, 0]).sort())
+    .toEqual([ [0,-1], [0,1], [-1,0], [1,0] ].sort())
+  })
+
+  it('all slots free on neighbour tiles', () => {
+    const fullCity = { sides: ['c', 'c', 'c', 'c'] }
+
+    const grid = new Grid({
+      [String([0,0])]: fullCity,
+      [String([1,0])]: fullCity
+    })
+
+    expect(Scoring.freeSlots(grid, [1, 0]).sort())
+    .toEqual([ [0,-1], [0,1], [-1,0], [1,0] ].sort())
+  })
+
+  it('no slots free on tile with meeple', () => {
+    const meepleCity = {
+      sides: ['c', 'c', 'c', 'c'],
+      meeple: { position: [0, 1] }
+    }
+
+    const grid = new Grid({
+      [String([0,0])]: meepleCity,
+    })
+
+    expect(Scoring.freeSlots(grid, [0, 0]).sort())
+    .toEqual([].sort())
+  })
+
+  it('no slots free on neighbour tiles with meeple', () => {
+    const fullCity = { sides: ['c', 'c', 'c', 'c'] }
+    const meepleCity = {
+      sides: ['c', 'c', 'c', 'c'],
+      meeple: { position: [0, 1] }
+    }
+
+    const grid = new Grid({
+      [String([0,0])]: meepleCity,
+      [String([1,0])]: fullCity
+    })
+
+    expect(Scoring.freeSlots(grid, [1, 0]).sort())
+    .toEqual([].sort())
+  })
+})
 
 describe('meepleSlots', () => {
 
