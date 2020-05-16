3f6197e6a649355c99fe63d86f75742e05d4be61
Matthew Benton <m@mattb.tech>
2020-05-16 09:42:13 +0100

    First pass zoom feature

---
+++ src/components/Zoom.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="zoom-control">
+    <button @click="$emit('zoom100')" class="zoom-button">100%</button>
+    <button @click="$emit('zoomSeeAll')" class="zoom-button">See all</button>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+.zoom-control {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: white;
+  z-index: 9999;
+}
+.zoom-button {
+  height: 32px;
+}
+</style>
